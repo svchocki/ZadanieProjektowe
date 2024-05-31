9682a5a081c3ffaa0985c23c5b884ab009294440
svchocki <145773118+svchocki@users.noreply.github.com>
2024-05-31 17:42:04 +0200

    Create WordDatabase Class.java

---
+++ Pseudocode/WordDatabase Class.java
@@ -0,0 +1,12 @@
+public class WordDatabase {
+    private List<String> words;
+
+    public WordDatabase() {
+    }
+
+    public String getRandomWord() {
+    }
+
+    public void addWord(String word) {
+    }
+}
