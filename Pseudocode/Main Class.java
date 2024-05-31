c677e4742b0af5a3f19f51209838b80bae36447d
svchocki <145773118+svchocki@users.noreply.github.com>
2024-05-31 17:40:52 +0200

    Create Main Class.java

---
+++ Pseudocode/Main Class.java
@@ -0,0 +1,9 @@
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
