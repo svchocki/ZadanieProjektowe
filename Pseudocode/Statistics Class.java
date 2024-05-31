26e812873b60a3f199642b06f89435f8bc941c59
svchocki <145773118+svchocki@users.noreply.github.com>
2024-05-31 17:42:41 +0200

    Create Statistics Class.java

---
+++ Pseudocode/Statistics Class.java
@@ -0,0 +1,13 @@
+public class Statistics {
+    private int wins;
+    private int losses;
+
+    public void recordWin() {
+    }
+
+    public void recordLoss() {
+    }
+
+    public String getStatistics() {
+    }
+}
