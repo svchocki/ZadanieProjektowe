500f767717ead1a8bae07bf66be690be38739823
svchocki <145773118+svchocki@users.noreply.github.com>
2024-05-31 17:48:24 +0200

    Create WordDatabase.java

---
+++ Implementation/Complete Code/WordDatabase.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class WordDatabase {
+    private List<String> words;
+
+    public WordDatabase() {
+        words = new ArrayList<>();
+        loadWords();
+    }
+
+    private void loadWords() {
+        // Add at least 10 words to the list
+        words.add("komputer");
+        words.add("zamek");
+        words.add("lampa");
+        words.add("kontroler");
+        words.add("ksiazka");
+        words.add("algorytm");
+        words.add("technologia");
+        words.add("kontynent");
+        words.add("telewizor");
+        words.add("nauka");
+    }
+
+    public String getRandomWord() {
+        Random random = new Random();
+        return words.get(random.nextInt(words.size()));
+    }
+
+    public void addWord(String word) {
+        words.add(word);
+    }
+}
