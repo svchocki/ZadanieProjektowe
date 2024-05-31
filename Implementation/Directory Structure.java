0f67fa407711c65033de79c99581b850febecb44
svchocki <145773118+svchocki@users.noreply.github.com>
2024-05-31 17:44:45 +0200

    Create Directory Structure.java

---
+++ Implementation/Directory Structure.java
@@ -0,0 +1,12 @@
+Hangman
+├── src
+│   ├── main
+│   │   └── Main.java
+│   ├── gui
+│   │   └── MainWindow.java
+│   ├── model
+│   │   ├── Game.java
+│   │   └── WordDatabase.java
+│   ├── util
+│   │   └── Statistics.java
+└── README.md
