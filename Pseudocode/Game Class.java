ce1e8e2bfafcdfd4f69cb8a847f735b4048462c2
svchocki <145773118+svchocki@users.noreply.github.com>
2024-05-31 17:41:29 +0200

    Create Game Class.java

---
+++ Pseudocode/Game Class.java
@@ -0,0 +1,12 @@
+public class Game {
+    private String currentWord;
+    private int attemptsLeft;
+    private Set<Character> guessedLetters;
+    
+    public void startNewGame() {
+    }
+
+    public boolean guessLetter(char letter) {
+    }
+
+}
